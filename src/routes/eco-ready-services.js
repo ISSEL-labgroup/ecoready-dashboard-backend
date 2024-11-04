f5604fefd9c52d427e1f6a7ac63325872debcb87
Thanos <atmichai@physics.auth.gr>
2024-11-04 17:21:07 +0200

    code cleanup

--- src/routes/eco-ready-services.js
+++ src/routes/eco-ready-services.js
@@ -1,13 +1,20 @@
 import express from "express";
 import Sentry from "@sentry/node";
+import dotenv from "dotenv";
 
 import { CollectionDataManagement } from "../utils/index.js";
 
+dotenv.config();
+
 const router = express.Router({ mergeParams: true });
+
+// Parse the ACCESS_KEY environment variable as a JSON object
+const accessKeys = JSON.parse(process.env.ACCESS_KEY);
 
 router.get("/getdata", async (req, res) => {
 	try {
-		const { organization, project, collection, accessKey, params } = req.query;
+		const { organization, project, collection, params } = req.query;
+		const accessKey = accessKeys[organization];
 		// console.log('Params in endpoint', params)
 		const response = await CollectionDataManagement.getData(organization, project, collection, accessKey, params);
 		return res.json(response);
@@ -23,7 +30,8 @@ router.get("/getdatastatistics", async (req, res) => {
 	// const collection = 'sensors_data';
 	// const accessKey = '******';
 	try {
-		const { organization, project, collection, accessKey, params } = req.query;
+		const { organization, project, collection, params } = req.query;
+		const accessKey = accessKeys[organization];
 		const response = await CollectionDataManagement.getDataStatistics(organization, project, collection, accessKey, params);
 		return res.json(response);
 	} catch (error) {
