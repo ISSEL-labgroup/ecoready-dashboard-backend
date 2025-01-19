c41888453c1539d2f5f8acf12061d8c1a88995ac
Thanos <atmichai@physics.auth.gr>
2025-01-19 19:06:27 +0200

    modified to require only universal key and added getcollections call

--- src/routes/eco-ready-services.js
+++ src/routes/eco-ready-services.js
@@ -2,19 +2,32 @@ import express from "express";
 import Sentry from "@sentry/node";
 import dotenv from "dotenv";
 
-import { CollectionDataManagement } from "../utils/index.js";
+import { CollectionManagement, CollectionDataManagement } from "../utils/index.js";
 
 dotenv.config();
 
 const router = express.Router({ mergeParams: true });
 
 // Parse the ACCESS_KEY environment variable as a JSON object
-const accessKeys = JSON.parse(process.env.ACCESS_KEY);
+// const accessKeys = JSON.parse(process.env.ACCESS_KEY);
+const universalAccessKey = process.env.UNIVERSAL_ACCESS_KEY;
+
+router.get("/getcollections", async (req, res) => {
+	try {
+		const { organization, project } = req.query;
+		const accessKey = universalAccessKey;
+		const response = await CollectionManagement.getCollections(organization, project, accessKey);
+		return res.json(response);
+	} catch (error) {
+		Sentry.captureException(error);
+		return res.status(500).json({ message: "Something went wrong." });
+	}
+});
 
 router.get("/getdata", async (req, res) => {
 	try {
 		const { organization, project, collection, params } = req.query;
-		const accessKey = accessKeys[organization];
+		const accessKey = universalAccessKey;
 		const response = await CollectionDataManagement.getData(organization, project, collection, accessKey, params);
 		return res.json(response);
 	} catch (error) {
@@ -26,7 +39,7 @@ router.get("/getdata", async (req, res) => {
 router.get("/getdatastatistics", async (req, res) => {
 	try {
 		const { organization, project, collection, params } = req.query;
-		const accessKey = accessKeys[organization];
+		const accessKey = universalAccessKey;
 		const response = await CollectionDataManagement.getDataStatistics(organization, project, collection, accessKey, params);
 		return res.json(response);
 	} catch (error) {
